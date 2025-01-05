f47939a05ef3eb33ee6b6926f15e18f08e752663
SophiedeVirieu <167359614+SophiedeVirieu@users.noreply.github.com>
2025-01-05 23:00:30 +0100

    doc Crab.java

--- src/main/java/org/example/ecosystemfx/Animals/Crab.java
+++ src/main/java/org/example/ecosystemfx/Animals/Crab.java
@@ -6,6 +6,16 @@ import org.example.ecosystemfx.base.Terrain2D;
 
 public class Crab extends Animal {
 
+    /**
+     * Constructor for the Crab class.
+     * 
+     * - Initializes a Crab instance with the specified coordinates (x, y).
+     * - Sets its attributes : species, predators, allowed terrains, possible food, speed, and calories.
+     * - Ensures that the position is on valid terrain using setY(y).
+     * 
+     * @param x The initial x coordinate.
+     * @param y The initial y coordinate.
+     */
     public Crab(int x, int y) {
         super(x, y);
         this.specie = species.CRAB;
@@ -17,15 +27,31 @@ public class Crab extends Animal {
         this.myFood.add(TerrainResources.food.FISH);
 
         this.speed = 2;
-        this.camouflage = 3;
         this.calories = 4;
 
         setY(y);
 
     }
+
+    /**
+     * Returns the color associated with the Crab.
+     * 
+     * - The color is defined as black.
+     * 
+     * @return A Color object representing the crab's color.
+     */
     @Override
     public Color getColor() {return new Color(0 / 255.0, 0 / 255.0, 0 / 255.0, 1);}
 
+    /**
+     * Handles the reproduction behavior of the Crab.
+     * 
+     * - Checks if the terrain adjacent to the current position (x+1, y) is valid
+     *   for the crab.
+     * - If valid, creates a new Crab instance at the adjacent position and adds it
+     *   to the simulation.
+     * - Calls the superclass `reproduce` method to lower satiety.
+     */
     @Override
     public void reproduce() {
         if (this.ground.contains(Terrain2D.getTerrain(this.x +1, this.y))) {
