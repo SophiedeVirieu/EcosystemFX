12955232488ca77ace9ba80d0b12ef7a997ec589
SophiedeVirieu <167359614+SophiedeVirieu@users.noreply.github.com>
2025-01-05 23:03:02 +0100

    doc Deer.java

--- src/main/java/org/example/ecosystemfx/Animals/Deer.java
+++ src/main/java/org/example/ecosystemfx/Animals/Deer.java
@@ -4,6 +4,16 @@ import org.example.ecosystemfx.Resources.TerrainResources;
 import org.example.ecosystemfx.base.Simulation;
 import org.example.ecosystemfx.base.Terrain2D;
 
+/**
+     * Constructor for the Deer class.
+     * 
+     * - Initializes a Crab instance with the specified coordinates (x, y).
+     * - Sets its attributes : species, predators, allowed terrains, possible food, speed, and calories.
+     * - Ensures that the position is on valid terrain using setY(y).
+     * 
+     * @param x The initial x coordinate.
+     * @param y The initial y coordinate.
+     */
 public class Deer extends Animal {
     public Deer(int x, int y) {
         super(x, y);
@@ -16,14 +26,30 @@ public class Deer extends Animal {
         this.ground.add(grounds.PLAIN);
 
         this.speed = 4;
-        this.camouflage = 2;
         this.calories = 8;
 
         setY(y);
     }
+
+    /**
+     * Returns the color associated with the Deer.
+     * 
+     * - The color is defined as brown.
+     * 
+     * @return A `Color` object representing the deer's color.
+     */
     @Override
     public Color getColor() {return new Color(139 / 255.0, 69 / 255.0, 19 / 255.0, 1);}
 
+    /**
+     * Handles the reproduction behavior of the Deer.
+     * 
+     * - Checks if the terrain adjacent to the current position (x+1, y) is valid
+     *   for the deer.
+     * - If valid, creates a new Deer instance at the adjacent position and adds it
+     *   to the simulation.
+     * - Calls the superclass `reproduce` method to lower satiety.
+     */
     @Override
     public void reproduce() {
         if (this.ground.contains(Terrain2D.getTerrain(this.x +1, this.y))) {
