ad7897bc0986318e73e53d3bfc1efaa05052d69d
SophiedeVirieu <167359614+SophiedeVirieu@users.noreply.github.com>
2025-01-05 23:23:09 +0100

    doc Seagull.java

--- src/main/java/org/example/ecosystemfx/Animals/Seagull.java
+++ src/main/java/org/example/ecosystemfx/Animals/Seagull.java
@@ -6,6 +6,16 @@ import org.example.ecosystemfx.base.Terrain2D;
 
 public class Seagull extends Animal {
 
+    /**
+     * Constructor for the Seagull class.
+     * 
+     * - Initializes a Seagull instance with the specified coordinates (x, y).
+     * - Sets its attributes: species, preys, predators, possible food, allowed terrains, speed, and calories.
+     * - Ensures that the position is on valid terrain using `setY(y)`.
+     * 
+     * @param x The initial x coordinate.
+     * @param y The initial y coordinate.
+     */
     public Seagull(int x, int y) {
         super(x, y);
         this.specie = species.SEAGULL;
@@ -21,14 +31,30 @@ public class Seagull extends Animal {
         this.ground.add(grounds.PLAIN);
 
         this.speed = 3;
-        this.camouflage = 1;
         this.calories = 4;
 
         setY(y);
     }
+
+    /**
+     * Returns the color associated with the Seagull.
+     * 
+     * - The color is defined as white.
+     * 
+     * @return A `Color` object representing the seagull's color.
+     */
     @Override
     public Color getColor() {return new Color(255 / 255.0, 255 / 255.0, 255 / 255.0, 1);}
 
+    /**
+     * Handles the reproduction behavior of the Seagull.
+     * 
+     * - Checks if the terrain adjacent to the current position (x+1, y) is valid
+     *   for the seagull.
+     * - If valid, creates a new Seagull instance at the adjacent position and adds it
+     *   to the simulation.
+     * - Calls the superclass `reproduce` method to lower satiety.
+     */
     @Override
     public void reproduce() {
         if (this.ground.contains(Terrain2D.getTerrain(this.x +1, this.y))) {
