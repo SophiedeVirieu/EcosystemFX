ba671c38c181fd689ea51ac09ef4f6fe84bc56e5
SophiedeVirieu <167359614+SophiedeVirieu@users.noreply.github.com>
2025-01-05 23:08:28 +0100

    doc Hedgehog.java

--- src/main/java/org/example/ecosystemfx/Animals/Hedgehog.java
+++ src/main/java/org/example/ecosystemfx/Animals/Hedgehog.java
@@ -7,6 +7,16 @@ import org.example.ecosystemfx.base.Terrain2D;
 
 public class Hedgehog extends Animal {
 
+    /**
+     * Constructor for the Hedgehog class.
+     * 
+     * - Initializes a Hedgehog instance with the specified coordinates (x, y).
+     * - Sets its attributes: species, predators, possible food, allowed terrains, speed, camouflage, and calories.
+     * - Ensures that the position is on valid terrain using `setY(y)`.
+     * 
+     * @param x The initial x coordinate.
+     * @param y The initial y coordinate.
+     */
     public Hedgehog(int x, int y) {
         super(x, y);
         this.specie = species.HEDGEHOG;
@@ -19,14 +29,30 @@ public class Hedgehog extends Animal {
         this.ground.add(grounds.PLAIN);
 
         this.speed = 1;
-        this.camouflage = 2;
         this.calories = 5;
 
         setY(y);
     }
+
+    /**
+     * Returns the color associated with the Hedgehog.
+     * 
+     * - The color is defined as dark brown.
+     * 
+     * @return A `Color` object representing the hedgehog's color.
+     */
     @Override
     public Color getColor() {return new Color(105 / 255.0, 42 / 255.0, 42 / 255.0, 1);}
 
+    /**
+     * Handles the reproduction behavior of the Hedgehog.
+     * 
+     * - Checks if the terrain adjacent to the current position (x+1, y) is valid
+     *   for the hedgehog.
+     * - If valid, creates a new Hedgehog instance at the adjacent position and adds it
+     *   to the simulation.
+     * - Calls the superclass `reproduce` method to lower satiety.
+     */
     @Override
     public void reproduce() {
         if (this.ground.contains(Terrain2D.getTerrain(this.x +1, this.y))) {
