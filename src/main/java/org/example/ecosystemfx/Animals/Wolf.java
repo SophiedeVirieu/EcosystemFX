dfd01fc5b4550234bb369a769366810fb9bbfbe7
SophiedeVirieu <167359614+SophiedeVirieu@users.noreply.github.com>
2025-01-05 23:27:34 +0100

    doc Wolf.java

--- src/main/java/org/example/ecosystemfx/Animals/Wolf.java
+++ src/main/java/org/example/ecosystemfx/Animals/Wolf.java
@@ -6,6 +6,16 @@ import org.example.ecosystemfx.base.Terrain2D;
 
 public class Wolf extends Animal {
 
+    /**
+     * Constructor for the Wolf class.
+     * 
+     * - Initializes a Wolf instance with the specified coordinates (x, y).
+     * - Sets its attributes: species, preys, allowed terrains, speed, and possible food.
+     * - Ensures that the position is on valid terrain using `setY(y)`.
+     * 
+     * @param x The initial x coordinate.
+     * @param y The initial y coordinate.
+     */
     public Wolf(int x, int y) {
         super(x, y);
         this.specie = species.WOLF;
@@ -16,14 +26,29 @@ public class Wolf extends Animal {
         this.ground.add(grounds.PLAIN);
 
         this.speed = 4;
-        this.camouflage = 3;
 
         setY(y);
     }
 
+    /**
+     * Returns the color associated with the Wolf.
+     * 
+     * - The color is defined as a slate gray.
+     * 
+     * @return A `Color` object representing the wolf's color.
+     */
     @Override
     public Color getColor() {return new Color(112 / 255.0, 128 / 255.0, 144 / 255.0, 1);}
 
+    /**
+     * Handles the reproduction behavior of the Wolf.
+     * 
+     * - Checks if the terrain adjacent to the current position (x+1, y) is valid
+     *   for the wolf.
+     * - If valid, creates a new Wolf instance at the adjacent position and adds it
+     *   to the simulation.
+     * - Calls the superclass `reproduce` method to lower satiety.
+     */
     @Override
     public void reproduce() {
         if (this.ground.contains(Terrain2D.getTerrain(this.x +1, this.y))) {
