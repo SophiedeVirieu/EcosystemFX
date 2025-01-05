ef26b0eb4ded348b6bb3eb298f4145a66bd1e80c
SophiedeVirieu <167359614+SophiedeVirieu@users.noreply.github.com>
2025-01-05 23:18:55 +0100

    doc Humans.java

--- src/main/java/org/example/ecosystemfx/Animals/Humans.java
+++ src/main/java/org/example/ecosystemfx/Animals/Humans.java
@@ -6,6 +6,16 @@ import org.example.ecosystemfx.base.Terrain2D;
 
 public class Humans extends Animal {
 
+    /**
+     * Constructor for the Humans class.
+     * 
+     * - Initializes a Humans instance with the specified coordinates (x, y).
+     * - Sets its attributes: species, prey, allowed terrains, possible food, speed, and calories.
+     * - Ensures that the position is on valid terrain using `setY(y)`.
+     * 
+     * @param x The initial x coordinate.
+     * @param y The initial y coordinate.
+     */
     public Humans(int x, int y) {
         super(x, y);
         this.specie = species.HUMAN;
@@ -21,15 +31,31 @@ public class Humans extends Animal {
         this.myFood.add(TerrainResources.food.BERRIES);
 
         this.speed = 3;
-        this.camouflage = 2;
         this.calories = 12;
 
         setY(y);
 
     }
+
+    /**
+     * Returns the color associated with Humans.
+     * 
+     * - The color is defined as black.
+     * 
+     * @return A `Color` object representing the humans' color.
+     */
     @Override
     public Color getColor() {return new Color(0 / 255.0, 0 / 255.0, 0 / 255.0, 1);}
 
+    /**
+     * Handles the reproduction behavior of Humans.
+     * 
+     * - Checks if the terrain adjacent to the current position (x+1, y) is valid
+     *   for humans.
+     * - If valid, creates a new Humans instance at the adjacent position and adds it
+     *   to the simulation.
+     * - Calls the superclass `reproduce` method to lower satiety.
+     */
     @Override
     public void reproduce() {
         if (this.ground.contains(Terrain2D.getTerrain(this.x +1, this.y))) {
